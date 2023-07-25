7ee53d40d4bb0660c1f1392935852e9b03a653c7
Andrew Wilborn <andrew.j.wilborn@sbcglobal.net>
2023-07-25 15:49:08 -0400

    ADDED animations

---
+++ src/components/BoxRace/BoxRace.jsx
@@ -0,0 +1,16 @@
+
+export default function BoxRace() {
+
+  return (
+    <>
+      <h2>BoxRace</h2>
+      <div className="boxrace-container">
+        <div className="boxrace linear">Linear</div>
+        <div className="boxrace ease">Ease</div>
+        <div className="boxrace ease-in">Ease In</div>
+        <div className="boxrace ease-out">Ease Out</div>
+        <div className="boxrace ease-in-out">Ease In Out</div>
+      </div>
+    </>
+  )
+}
